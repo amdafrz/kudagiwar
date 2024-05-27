5e348892657614de4d838949ad360566bc6b632c
Ahmad Al - Farizi <92077482+amdafrz@users.noreply.github.com>
2024-05-27 11:43:06 +0700

    tubes.py

---
+++ tubes.py
@@ -0,0 +1,10 @@
+daftar_buku = {
+  "Kita Bersama" :
+  "Sabi : Santai di Bali" :
+  "Evaluasi" :
+  "Padahal Kita Sama" :
+  "Kenapa Harus Setara?" :
+  "Dirimu Diriku Diriku Dirimu" :
+  "Secukupnya Saja" :
+  "Pernah Ada di Sampingku" :
+  "Nyata Namun Nyatanya Tiada" :
