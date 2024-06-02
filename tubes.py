4d02ed3b9e33156c84a9c4bc47b82bbabb9b2e5d
Ahmad Al - Farizi <92077482+amdafrz@users.noreply.github.com>
2024-06-02 21:36:05 +0700

    tubes.py

--- tubes.py
+++ tubes.py
@@ -50,24 +50,36 @@ def cari_buku():
     print("====================================================================")
     print("Hallo", nama_user + ", buku apa yang kamu cari hari ini ?")
     print("====================================================================")
-    for judul,kode in zip(judul_buku, kode_buku):
-      print(f"- Judul Buku : {judul.ljust(33)} - Kode Buku : {kode}")
+    for i in range (len(judul_buku)):
+        judul = judul_buku[i]
+        kode = kode_buku[i]
+        print(f"- Judul Buku : {judul.ljust(33)} - Kode Buku : {kode}")
+    print("====================================================================")
     while True:
+      input_user = input("Ketik Judul atau Kode Buku disini (Ketik 'tutup' untuk keluar): ")
       print("====================================================================")
-      input_user = input("Ketik Judul atau Kode Buku disini: ")
-      print("====================================================================")
-      if input_user in judul_buku:
-        kode = kode_buku[judul_buku.index(input_user)]
-        print("Judul Buku yang kamu cari:", input_user + ", dengan Kode :", kode)
-        print("====================================================================")
-        break
+      if input_user.lower() == "tutup":
+          print("Terima kasih telah mengunjungi Perpustakaan Kuda Giwar")
+          print("====================================================================")
+          return
+      elif input_user in judul_buku:
+          kode = kode_buku[judul_buku.index(input_user)]
+          print("Data peminjaman buku")
+          print("- Nama       :",nama_user) 
+          print("- Judul Buku :",input_user)
+          print("- Kode       :",kode)
+          print("====================================================================")
+          break
       elif input_user in kode_buku:
-        judul = judul_buku[kode_buku.index(input_user)]
-        print("Judul Buku yang kamu cari:", judul + ", dengan Kode:", input_user)
-        print("====================================================================")
-        break
+          judul = judul_buku[kode_buku.index(input_user)]
+          print("Data peminjaman buku")
+          print("- Nama       :",nama_user)
+          print("- Judul Buku :",judul)
+          print("- Kode       :",input_user)
+          print("====================================================================")
+          break
       else:
-        print("Tidak ada hasil, Ketik nama atau kode buku dengan benar.")
+          print("Tidak ada hasil, Ketik nama atau kode buku dengan benar.")
 cari_buku()
       
   
